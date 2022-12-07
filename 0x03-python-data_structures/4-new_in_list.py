20902b6a9095891b7ffbb3baf24a4ab26174666c
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-06 22:24:51 -0800

    Copy of a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    items = len(my_lis)
+    new_list = my_list[:]
+
+    if idx < 0 > items:
+        return my_list
+
+    new_list[idx] = element
+    return new_list
