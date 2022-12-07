a42bd0c1daf81cf7a824a6c00a699428045ce678
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-07 01:12:04 -0800

    Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        if item == row[-1]:
+            print("{:d}".format(item), end="")
+        else:
+            print("{:d}".format(item), end="")
+
+    print('')
