8708ccf227ee8c22b01e00d1e8441667df6d103a
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-06 22:44:48 -0800

    Create a copy of a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    items = len(my_list)
-
-    if idx < 0 > items:
+    items = len(my_list) - 1
+    if idx < 0 or idx > items:
         return my_list
     new_list = my_list.copy()
     new_list[idx] = element
