02d09c647983d62edfb979d65bcebb438846affa
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-08 03:23:49 -0800

    Palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,10 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -16,8 +15,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
