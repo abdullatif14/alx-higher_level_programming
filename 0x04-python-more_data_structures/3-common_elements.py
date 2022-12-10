69dc4771f375b206480698517c16a80309e6be6a
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-09 23:20:57 -0800

    Common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    result = []
+    for element in list1:
+        if element in set_1:
+            result.append(elements)
+    return result
