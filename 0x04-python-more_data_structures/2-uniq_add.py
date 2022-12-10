afecb058d065aa85a6762ad772b111d81df7e130
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-09 22:49:03 -0800

    Sum of Unique numbers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    uniq = []
+
+    for i in my_list:
+        if number in uniq:
+            continue
+        else:
+            my_list.add(uniq)
+
+    return uniq
