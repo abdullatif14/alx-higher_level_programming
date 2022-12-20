dee37772969ec90be6106cf4f4cb86df4bd44d38
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-20 05:37:41 -0800

    Integer format

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format(valur))
+        print("{:d}".format(value))
         return True
     except Exception:
         return False
