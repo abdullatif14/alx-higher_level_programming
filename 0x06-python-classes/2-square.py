5a94ad311593b05cacc970b02fde46fd144503ee
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-27 00:25:40 -0800

    Raising errors in Sqaure

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" Define a square """
+"""Defines a square based on 1-square.py"""
 
 
 class Square:
-    """ A class that defines a square"""
-    def __init__(self, size):
-        """ Initialize with size"""
+    """Instantiation would raise both TypeError and ValueError"""
+    def __init__(self, size=0):
+        self.__size = size
         if type(size) is not int:
-            raise TypeError("size must be an intger")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
