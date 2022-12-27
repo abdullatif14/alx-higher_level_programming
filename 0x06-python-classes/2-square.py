c688c534d4d69a5320164530b1d210fb4fd789b8
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-26 23:42:34 -0800

    Square which handles Errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,10 @@
 class Square:
     """ A class that defines a square"""
     def __init__(self, size):
-        """ Instantiation with size"""
-        if not isintance(size, int):
+        """ Initialize with size"""
+        if type(size) is not int:
             raise TypeError("size must be an intger")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
