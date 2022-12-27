d067b844aba5c07fd64acd1ee42eb08351515243
abdullatif14 <abdilatifmohamed345@gmail.com>
2022-12-27 00:44:48 -0800

    Use Setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Getting the square using getter and setter"""
+
+
+class Square:
+    """ Defines the Square class """
+    def __init__(self, size=0):
+        """ initialize size """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
