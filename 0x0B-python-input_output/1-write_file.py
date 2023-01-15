e5e793a83058f7c90577beb5d749ed6ef7c5276f
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-01-14 22:41:43 -0800

    Writes a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Function that writes a string to a text file """
+
+
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="utf-8") as file:
+        count = file.write(text)
+
+    return count
