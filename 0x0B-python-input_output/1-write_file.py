678b47dca8ac01a87ff1779ab29849fb7be3c2d5
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-01-14 22:43:27 -0800

    Writes a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,4 @@
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="utf-8") as file:
-        count = file.write(text)
-
-    return count
+        return file.write(text)
