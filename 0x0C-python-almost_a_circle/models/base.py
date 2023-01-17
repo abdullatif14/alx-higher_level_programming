6937c7c8b39270f79d020adaaa4327e4f58a6dc9
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-01-17 00:06:09 -0800

    Creating base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Create a class 'base' of all other classes in this project """
+
+
+class Base:
+    """ starting to create class base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initialising class
+        constructor that takes
+        an optional parameter 'id'
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
