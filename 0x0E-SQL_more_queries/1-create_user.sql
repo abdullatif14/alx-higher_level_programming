2b50e4dcf9771dff34d0880c2e568555fb199a7e
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-02-18 10:17:19 -0800

    Create User

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create MySQL server user
-
+-- a script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PREVILEGES ON *.* TO 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
