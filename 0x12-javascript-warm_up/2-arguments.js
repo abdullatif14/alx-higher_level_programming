e0a406b77785de9326ad2d9cae1561dd1c256102
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-03-16 06:44:31 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === ? 'No argument' : count === 3? 'Argument found' : 'Argument found');
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+
