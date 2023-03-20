ddbcc7f4cf23f64c4f3ec8808e6fa4b54a46807a
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-03-20 03:44:00 -0700

    Imports an array and computes a new array

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const list = require('./100-data').list;
+
+const newList = list.map((value, index) => {
+  return value * index;
+});
+
+console.log(list);
+console.log(newList);
