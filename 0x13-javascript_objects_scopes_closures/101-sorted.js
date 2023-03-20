6756bb5632e8bd5c21f05cff5130545de9b3b809
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-03-20 03:49:52 -0700

    Import dict of occurences by user id

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value] === undefined) {
+    newDict[value] = [key];
+  } else {
+    newDict[value].push(key);
+  }
+}
+
+console.log(newDict);
